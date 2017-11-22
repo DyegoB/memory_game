b1fe0b0cec37d64c362a9daa767b08c1da7dc6ba
DyegoB <diego.bks@hotmail.com>
2017-11-21 22:24:49 -0200

    Applying modifications look at

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from kivy.app import App
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+import random
 
 kivy.require('1.10.0')
 class Jogo(BoxLayout):
@@ -22,13 +23,29 @@ class Jogo(BoxLayout):
 class Tela(StackLayout):
     def __init__(self, **kwargs):
         super(Tela, self).__init__(**kwargs)
+        self.list_id = []
+        x = 1
         for i in range(100):
             #create buttons in range(100) 0 to 99
-            self.add_widget(Button(text='Test', size_hint=(0.1, 0.1),
-                                   on_press=self.on_press_click))
+            #variavel ID = x #contador
+            bt = Button(id=str(x), text=str(x), size_hint=(0.1, 0.1))
+            bt.bind(on_press=self.on_press_click)
+            #bt.bind(on_release=self.on_release_click)
+            self.list_id.append(str(x))
+            x += 1
+            self.add_widget(bt)
+
+        #raffle pictures
+        list_img = ['poker.jpg', 'poker_1.jpg']
+        self.dc = dict()
+        for id in self.list_id:
+            img = random.choice(list_img)
+            self.dc[id] = img
     def on_press_click(self, bt):
-        #all the buttons created with for loop shared this function
-        print('test')
+        print(self.dc[bt.id])
+
+    def on_release_click(self, bt):
+        print('deselect')
 
 class MemoriaApp(App):
     def build(self):
